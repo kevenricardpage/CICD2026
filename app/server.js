3592c84b416f55c75f03cd924360d8f6eb173f5e
kevenricardpage <1209410@cegepmontpetit.ca>
2026-04-24 17:42:17 -0400

    dfgdgf

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 const ENV = process.env.APP_ENV || 'dev';
 
 const messages = {
-  dev: "Hello de l'environnement testeur",
+  dev: "Hello de l'environnement broutbrout",
   qa: "Hello de l'environnement de QA",
   prod: "Hello de l'environnement de PROD",
 };
